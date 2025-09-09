f551138ba7834820d883ebbcd743ebe72e5076f6
Mari <mariagiraldoo646@gmail.com>
2025-09-09 18:44:49 -0500

    Mi primer commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "com.example.anserview"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.example.anserview"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+    //Dependencias osmdroid
+    implementation("org.osmdroid:osmdroid-android:6.1.14")
+    implementation("org.osmdroid:osmdroid-wms:6.1.14")
+    //Dependencias de la libreria de google volley
+    implementation("com.android.volley:volley:1.2.1")
+
+    //Gestion de Imagenes
+    implementation("com.github.bumptech.glide:glide:4.15.1")
+
+    //Animaciones
+    implementation("com.airbnb.android:lottie:6.0.0")
+}
---
+++ app/src/main/java/com/example/anserview/ActivityInicioSesion.java
@@ -0,0 +1,16 @@
+package com.example.anserview;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class ActivityInicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+
+    }
+}
